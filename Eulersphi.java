b58532db753181db982595c6e7fefad6f86350a8
SRINJOY DAS <srinjoydas1104@gmail.com>
2024-11-30 00:01:07 +0530

    comms

--- Eulersphi.java
+++ Eulersphi.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-// 
+// phi(2) jodi bole tale  > like gdc(1,2) dakho :  1 hole good , and gcd(2,2)  = 2 not good so phi(n) = gcd number of one counts  = 1
 public class Eulersphi {
     static int gcd(int a , int b) {
         if(a == 0) {
